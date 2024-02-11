afbcf4bd1fb10821e1c2497a88b74eba7dfe00cf
inedibleProgrammer <inedibleProgrammer@gmail.com>
2024-02-10 21:30:56 -0500

    Add comment about linux/windows version

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
 
-
+# Make this script's directory the current directory
 SCRIPT_DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
 
 
-
-
+cp ./.emacs ~/
+# cp ./.tmux.conf ~/
+# cp ./.vimrc ~/
+# TODO: Something different for linux
+cp -r ./helix $HOME/AppData/Roaming
 
